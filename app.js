fbb7a467ea50b48326e3db912c3d3c944a9c5a9d
Sayed Abdo <Sayedabdo4900@gmail.com>
2022-12-28 13:14:09 +0200

    shop

---
+++ app.js
@@ -0,0 +1,11 @@
+const link = document.getElementsByClassName("link");
+let currentValue = 1;
+
+function activeLink() {
+    for (l of link) {
+        l.classList.remove("active");
+    }
+
+    event.target.classList.add("active");
+    currentValue = event.target.value;
+}
