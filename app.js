4583a363371e8faab5253200b7679a454547312f
Sayed Abdo <Sayedabdo4900@gmail.com>
2022-12-28 20:04:47 +0200

    finish detals

--- app.js
+++ app.js
@@ -9,3 +9,40 @@ function activeLink() {
     event.target.classList.add("active");
     currentValue = event.target.value;
 }
+
+function backBtn() {
+    if (currentValue > 1) {
+        for (l of link) {
+            l.classList.remove("active");
+        }
+        currentValue--;
+        link[currentValue - 1].classList.add("active");
+    }
+}
+function nextBtn() {
+    if (currentValue < 1) {
+        for (l of link) {
+            l.classList.remove("active");
+        }
+        currentValue++;
+        link[currentValue - 1].classList.add("active");
+    }
+}
+
+const mainImg = document.getElementById("mainImg");
+
+const smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function () {
+    mainImg.src = smallImg[0].src;
+    console.log("1");
+};
+smallImg[1].onclick = function () {
+    mainImg.src = smallImg[1].src;
+    console.log("2");
+};
+
+smallImg[2].onclick = function () {
+    mainImg.src = smallImg[2].src;
+    console.log("3");
+};
